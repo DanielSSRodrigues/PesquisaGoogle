f53f97fd914e1a82e256642486fe261966a5d689
Daniel Rodrigues <danieldssr31@gmail.com>
2023-04-05 11:50:55 -0300

    atualiza

--- PesquisaGoogle/TestePesquisaGoogle.cs
+++ PesquisaGoogle/TestePesquisaGoogle.cs
@@ -10,43 +10,43 @@ namespace PesquisaGoogle
 {
     public class TestePesquisaGoogle : IDisposable
     {
-        private readonly IWebDriver _driver;
+        private readonly IWebDriver navegador;
         private readonly string _url;
 
         public TestePesquisaGoogle()
         {
             _url = "https://www.google.com.br/";
-            _driver = new ChromeDriver();
+            navegador = new ChromeDriver();
         }
 
         [Fact]
         public void PesquisarTenisNoGoogle()
         {
             // Acessa o site do Google
-            _driver.Navigate().GoToUrl(_url);
+            navegador.Navigate().GoToUrl(_url);
             Thread.Sleep(2000);
 
             // Encontra o campo de pesquisa e insere o termo "tênis"
-            var campoPesquisa = _driver.FindElement(By.Name("q"));
+            var campoPesquisa = navegador.FindElement(By.Name("q"));
             campoPesquisa.SendKeys("tênis");
             Thread.Sleep(2000);
 
             // Encontra o botão de pesquisa e clica nele
-            var botaoPesquisa = _driver.FindElement(By.Name("btnK"));
+            var botaoPesquisa = navegador.FindElement(By.Name("btnK"));
             botaoPesquisa.Click();
             Thread.Sleep(2000);
 
             // Verifica se a página de resultados de pesquisa foi carregada
             var tituloEsperado = "tênis - Pesquisa Google";
-            _driver.Title.Should().Be(tituloEsperado, $"O título atual é {_driver.Title}");
+            navegador.Title.Should().Be(tituloEsperado, $"O título atual é {navegador.Title}");
             Thread.Sleep(2000);
 
         }
 
         public void Dispose()
         {
-            _driver.Quit();
-            _driver.Dispose();
+            navegador.Quit();
+            navegador.Dispose();
         }
     }
 }
